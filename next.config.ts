8e2e074cce5b20cf56d632dc038309b69bb2a205
dimitri mcdaniel <dimitri@circleclick.com>
2025-03-27 06:46:05 -0400

    updating next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "app"),
+      "@/components": path.resolve(__dirname, "app/components"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
